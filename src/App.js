4ca1d9d823b5dbf52b71dfe4b3373024b079eaad
delanobmarques <delanobmarques@gmail.com>
2022-07-21 19:45:03 -0300

    create global styles, basic routes, and Hero component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+
+//PAGES
+import Home from "./pages/Home.page";
+import Pricing from "./pages/Pricing.page";
+import SignUp from "./pages/SignUp.page";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Delano</h1>
-    </div>
+    <Router>
+      <GlobalStyle />
+      
+      <Routes>  
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+
+      </Routes>
+    </Router>
   );
 }
 
